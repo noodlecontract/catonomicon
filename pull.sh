a6e2071f86e850dd1fa452b208e7485f333ae20d
alex <noodlecontract@gmail.com>
2022-06-07 12:14:47 -0400

    update ipfs

--- pull.sh
+++ pull.sh
@@ -14,8 +14,8 @@ max=${1:-20}
 # Download from ipfs
 # ==========================================
 mkdir -p output_raw
-# base_url="cloudflare-ipfs.com/ipfs/QmZeFg4o5brY4DzsG9TAXtai7trKouMA8w5i1J9oAwbMpw"
-base_url="https://opensea.mypinata.cloud/ipfs/QmZeFg4o5brY4DzsG9TAXtai7trKouMA8w5i1J9oAwbMpw"
+# base_url="cloudflare-ipfs.com/ipfs/Qmdbto5EXSFX659mDbRSgQr3T8RjqTFfH3oFB8mCbNJdab"
+base_url="https://opensea.mypinata.cloud/ipfs/Qmdbto5EXSFX659mDbRSgQr3T8RjqTFfH3oFB8mCbNJdab"
 # lots of pomp + circumstance for light parallelism + "only download if entry does not exist or is unrevealed"
 seq 1 $max | xargs -P 4 -I {} sh -c "((test ! -e output_raw/{}.json) || grep -q 'placeholder.png' output_raw/{}.json) && (echo 'pulling {}'; curl --silent $base_url/{}.json | jq > output_raw/{}.json) || exit 0";
 
